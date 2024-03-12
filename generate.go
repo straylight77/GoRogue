91ef6fe25df8bdc5ea9733a026d7df6a84ee1a28
Colin Bowers <colin.bowers@gmail.com>
2024-03-12 17:49:09 -0400

    Added comments to each function including overview of the graph creation algo.

--- generate.go
+++ generate.go
@@ -6,17 +6,17 @@ import (
 
 var debug = DebugMessageLog{}
 
-// ----------------------------------------------------------------------------
-
 var graph *RoomGraph
 
+// ----------------------------------------------------------------------------
+// Entry point from main() to build a new dungeon level
 func generateRandomLevel(dm *DungeonMap, ml *MonsterList, p *Player) {
 	debug.Clear()
 	dm.Clear()
 
 	graph = newRandomGraph()
 
-	graph.getAreaBounds()
+	graph.MakeCellBounds()
 	graph.MakeRandomRooms()
 	x, y := buildMap(graph, dm)
 
@@ -25,6 +25,7 @@ func generateRandomLevel(dm *DungeonMap, ml *MonsterList, p *Player) {
 }
 
 // ----------------------------------------------------------------------------
+// Takes a completed RoomGraph and changes the tiles in DungeonMap appropriately
 func buildMap(g *RoomGraph, d *DungeonMap) (int, int) {
 
 	// create the rooms on the dungeon map
@@ -79,6 +80,17 @@ type RoomGraph struct {
 }
 
 // ----------------------------------------------------------------------------
+// Creates a new randomized room graph.  Follows the following steps:
+//  1. Pick a random room
+//  2. Connect it to a random neighbour
+//  3. While there are unconnected rooms remaining:
+//     a. Pick a random unconnected room
+//     b. Connect it to a random connected neighbour
+//     (This guarantees all rooms will end up connected together.)
+//     c. If there are no connected neighbours, skip it and continue looping.
+//     d. Limit the nubmer of loop to 20 as safeguard.
+//  4. Drop 2 of the rooms
+//  5. Check for dead ends and prune them (done by DropRandomRooms())
 func newRandomGraph() *RoomGraph {
 	g := RoomGraph{}
 
@@ -106,16 +118,31 @@ func newRandomGraph() *RoomGraph {
 }
 
 // ----------------------------------------------------------------------------
-func (g *RoomGraph) DropRandomRooms(count int) {
-	for i := 0; i < count; i++ {
-		cell := g.RandCell(1)
-		debug.Add("Dropping room %d", cell)
-		g.rooms[cell].mark = -1
-		g.PruneDeadends(cell)
+// Checks if there's a non-dropped path between the given cells
+func (g *RoomGraph) AreConnected(c1, c2 int) bool {
+	for _, p := range g.paths {
+		if p.mark != -1 &&
+			(p.origID == c1 || p.destID == c1) &&
+			(p.origID == c2 || p.destID == c2) {
+			//debug.Add("AreConnected: %d, %d, yes!", c1, c2)
+			return true
+		}
 	}
+	return false
 }
 
 // ----------------------------------------------------------------------------
+// Creates a path between the given cells
+func (g *RoomGraph) Connect(c1, c2 int) {
+	p := Path{origID: c1, destID: c2}
+	g.paths = append(g.paths, p)
+	g.rooms[c1].mark = 1
+	g.rooms[c2].mark = 1
+	//debug.Add("Connect(%d, %d) %v %v", c1, c2, g.rooms[c1], g.rooms[c2])
+}
+
+// ----------------------------------------------------------------------------
+// Returns the total number paths coming in or going out of the given cell
 func (g *RoomGraph) CountPaths(cell int) int {
 	count := 0
 	for _, p := range g.paths {
@@ -127,72 +154,7 @@ func (g *RoomGraph) CountPaths(cell int) int {
 }
 
 // ----------------------------------------------------------------------------
-func (g *RoomGraph) PruneDeadends(cell int) {
-	// check if the given cell is a dead end
-	if g.rooms[cell].mark == -1 && g.CountPaths(cell) == 1 {
-
-		// if it is, remove all the paths (should be just one)
-		for i, p := range g.paths {
-			if p.origID == cell || p.destID == cell {
-				g.paths[i].mark = -1
-				debug.Add("dropping path %v, cell=%d", p, cell)
-			}
-		}
-	}
-}
-
-// ----------------------------------------------------------------------------
-func (g *RoomGraph) Neighbours(cell int) []int {
-	ref := [9][]int{
-		{1, 3},       // cell 0
-		{0, 4, 2},    // cell 1
-		{1, 5},       // cell 2
-		{0, 4, 6},    // cell 3
-		{1, 5, 7, 3}, // cell 4
-		{2, 4, 8},    // cell 5
-		{3, 7},       // cell 6
-		{6, 4, 8},    // cell 7
-		{7, 5},       // cell 8
-	}
-	return ref[cell]
-}
-
-// ----------------------------------------------------------------------------
-func (g *RoomGraph) RandCell(mark int) int {
-	cells := []int{}
-
-	for i, r := range g.rooms {
-		if r.mark == mark {
-			cells = append(cells, i)
-		}
-	}
-	if len(cells) == 0 {
-		return -1
-	}
-
-	idx := rand.Intn(len(cells))
-	return cells[idx]
-}
-
-// ----------------------------------------------------------------------------
-func (g *RoomGraph) RandNeighbour(cell int, mark int) int {
-	nbList := []int{}
-
-	for _, nb := range g.Neighbours(cell) {
-		if g.rooms[nb].mark == mark {
-			nbList = append(nbList, nb)
-		}
-	}
-
-	if len(nbList) == 0 {
-		return -1
-	}
-
-	idx := rand.Intn(len(nbList))
-	return nbList[idx]
-}
-
-// ----------------------------------------------------------------------------
+// Gives the relative Direction when going from c1 to c2.  Used to build paths.
 func (g *RoomGraph) Direction(c1, c2 int) Direction {
 	col1, row1 := c1%3, c1/3
 	col2, row2 := c2%3, c2/3
@@ -217,29 +179,19 @@ func (g *RoomGraph) Direction(c1, c2 int) Direction {
 }
 
 // ----------------------------------------------------------------------------
-func (g *RoomGraph) Connect(c1, c2 int) {
-	p := Path{origID: c1, destID: c2}
-	g.paths = append(g.paths, p)
-	g.rooms[c1].mark = 1
-	g.rooms[c2].mark = 1
-	//debug.Add("Connect(%d, %d) %v %v", c1, c2, g.rooms[c1], g.rooms[c2])
-}
-
-// ----------------------------------------------------------------------------
-func (g *RoomGraph) AreConnected(c1, c2 int) bool {
-	for _, p := range g.paths {
-		if p.mark != -1 &&
-			(p.origID == c1 || p.destID == c1) &&
-			(p.origID == c2 || p.destID == c2) {
-			//debug.Add("AreConnected: %d, %d, yes!", c1, c2)
-			return true
-		}
+// Marks the given number of cells as dropped.  Assume they are already connected.
+func (g *RoomGraph) DropRandomRooms(count int) {
+	for i := 0; i < count; i++ {
+		cell := g.RandCell(1)
+		debug.Add("Dropping room %d", cell)
+		g.rooms[cell].mark = -1
+		g.PruneDeadends(cell)
 	}
-	return false
 }
 
 // ----------------------------------------------------------------------------
-func (g *RoomGraph) getAreaBounds() (areas []Room) {
+// Determines the boundaries of the area within each cell.  Used to randomize the rooms.
+func (g *RoomGraph) MakeCellBounds() (areas []Room) {
 
 	// assume 3x3 rooms on the map
 	roomW := (MapMaxX - 2) / 3 // 25
@@ -258,6 +210,8 @@ func (g *RoomGraph) getAreaBounds() (areas []Room) {
 }
 
 // ----------------------------------------------------------------------------
+// Create rooms in each cell with random size and location within the cell bounds.
+// Assumes the bounds have already been created.
 func (g *RoomGraph) MakeRandomRooms() {
 
 	// make a random room within each area
@@ -271,6 +225,79 @@ func (g *RoomGraph) MakeRandomRooms() {
 
 }
 
+// ----------------------------------------------------------------------------
+// Gives a slice of the neighbouring cells of a given cell.
+func (g *RoomGraph) Neighbours(cell int) []int {
+	ref := [9][]int{
+		{1, 3},       // cell 0
+		{0, 4, 2},    // cell 1
+		{1, 5},       // cell 2
+		{0, 4, 6},    // cell 3
+		{1, 5, 7, 3}, // cell 4
+		{2, 4, 8},    // cell 5
+		{3, 7},       // cell 6
+		{6, 4, 8},    // cell 7
+		{7, 5},       // cell 8
+	}
+	return ref[cell]
+}
+
+// ----------------------------------------------------------------------------
+// For a given cell, checks if it is a deadend (just one path with no room attached)
+// and removes the path if it is.
+func (g *RoomGraph) PruneDeadends(cell int) {
+
+	// check if the given cell is a dead end
+	if g.rooms[cell].mark == -1 && g.CountPaths(cell) == 1 {
+
+		// if it is, remove all the paths (should be just one)
+		for i, p := range g.paths {
+			if p.origID == cell || p.destID == cell {
+				g.paths[i].mark = -1
+				debug.Add("dropping path %v, cell=%d", p, cell)
+			}
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------
+// Returns a cell chosen at random with the given mark or -1 if none are available.
+func (g *RoomGraph) RandCell(mark int) int {
+	cells := []int{}
+
+	for i, r := range g.rooms {
+		if r.mark == mark {
+			cells = append(cells, i)
+		}
+	}
+	if len(cells) == 0 {
+		return -1
+	}
+
+	idx := rand.Intn(len(cells))
+	return cells[idx]
+}
+
+// ----------------------------------------------------------------------------
+// Returns a randomly chosen neighbour of the given cell with the given mark or
+// -1 if none are avilable.
+func (g *RoomGraph) RandNeighbour(cell int, mark int) int {
+	nbList := []int{}
+
+	for _, nb := range g.Neighbours(cell) {
+		if g.rooms[nb].mark == mark {
+			nbList = append(nbList, nb)
+		}
+	}
+
+	if len(nbList) == 0 {
+		return -1
+	}
+
+	idx := rand.Intn(len(nbList))
+	return nbList[idx]
+}
+
 /*****************************************************************************/
 
 type Room struct {
@@ -279,23 +306,26 @@ type Room struct {
 	mark int // 0=unconnected, 1=connected, -1=dropped
 }
 
-func (r *Room) SetSize(x, y, w, h int) {
-	r.X = x
-	r.Y = y
-	r.W = w
-	r.H = h
-}
-
+// Returns the screen coord of the room's center
 func (r Room) Center() (x int, y int) {
 	x = r.X + r.W/2
 	y = r.Y + r.H/2
 	return
 }
 
+// Returns a random point within the room ensuring it's not on a wall
 func (r Room) RandPoint() (x int, y int) {
 	x = r.X + rand.Intn(r.W-2) + 1
 	y = r.Y + rand.Intn(r.H-2) + 1
 	return
+}
+
+// Updates the dimensions of the room
+func (r *Room) SetSize(x, y, w, h int) {
+	r.X = x
+	r.Y = y
+	r.W = w
+	r.H = h
 }
 
 /*****************************************************************************/
