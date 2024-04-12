ea7b6e58a64d1db255110549e2956db4a5292ca1
Colin Bowers <colin.bowers@gmail.com>
2024-04-11 21:01:02 -0400

    Added various probabilities for each potion to be generated randomly.  This makes potions feature complete.

--- items.go
+++ items.go
@@ -334,6 +334,8 @@ func randArmor() *Item {
 // val1: index of PotionLib for this potion
 
 type PotionTemplate struct {
+	pct        int // probability of this potion being randomly generated
+	chance     int // cumulative probability
 	name       string
 	effect     int
 	color      int
@@ -342,22 +344,20 @@ type PotionTemplate struct {
 }
 
 var PotionLib = []PotionTemplate{
-	{"thirst quenching", E_Nothing, 0, false, "Meh, tastes pretty dull."},
-	{"healing", E_Healing, 0, false, "You begin to feel better."},
-	{"extra healing", E_ExtraHealing, 0, false, "You begin to feel much better."},
-	{"strength", E_Strength, 0, false, "You feel stronger, what bulging muscles!"},
-	{"poison", E_Poison, 0, false, "You feel very sick now."},
-	{"blindness", E_Blindness, 0, false, "A cloak of darkness falls around you."},
-	{"confusion", E_Confusion, 0, false, "Wait, what's going on here. Huh? What? Who?"},
-	{"restore strength", E_Restore, 0, false, "Hey, this tastes great, it make you feel warm all over."},
-	{"detect magic", E_DetMagic, 0, false, "You sense the presence of magic."},
-	{"monster detection", E_DetMonsters, 0, false, "You feel like you are not alone."},
-	{"raise level", E_LevelUp, 0, false, "You feel more experienced."},
-	{"paralysis", E_Paralyze, 0, false, "You feel your body seizing up, you can't move!"},
-	{"haste", E_Haste, 0, false, "Tastes like coffee, everything seems to slow down."},
-	{"truesight", E_Truesight, 0, false, "Tastes like slime-mold juice."},
-	//see invisible
-	//haste
+	{15, 15, "healing", E_Healing, 0, false, "You begin to feel better."},
+	{15, 30, "strength", E_Strength, 0, false, "You feel stronger, what bulging muscles!"},
+	{14, 44, "restore strength", E_Restore, 0, false, "Hey, this tastes great, it make you feel warm all over."},
+	{10, 54, "paralysis", E_Paralyze, 0, false, "You feel your body seizing up, you can't move!"},
+	{8, 62, "confusion", E_Confusion, 0, false, "Wait, what's going on here. Huh? What? Who?"},
+	{8, 70, "poison", E_Poison, 0, false, "You feel very sick now."},
+	{6, 76, "monster detection", E_DetMonsters, 0, false, "You feel like you are not alone."},
+	{6, 82, "detect magic", E_DetMagic, 0, false, "You sense the presence of magic."},
+	{5, 87, "extra healing", E_ExtraHealing, 0, false, "You begin to feel much better."},
+	{4, 91, "haste", E_Haste, 0, false, "Tastes like coffee, everything seems to slow down."},
+	{4, 95, "blindness", E_Blindness, 0, false, "A cloak of darkness falls around you."},
+	{2, 97, "raise level", E_LevelUp, 0, false, "You feel more experienced."},
+	{2, 99, "truesight", E_Truesight, 0, false, "Tastes like slime-mold juice."},
+	{1, 100, "thirst quenching", E_Nothing, 0, false, "Meh, tastes pretty dull."},
 }
 
 var PotionColors = []string{
@@ -426,8 +426,16 @@ func newPotion(name string) *Item {
 }
 
 func randPotion() *Item {
-	roll := rand.Intn(len(PotionLib))
-	return newPotion(PotionLib[roll].name)
+	roll := rand.Intn(100) + 1 //1-100
+	name := ""
+	for _, t := range PotionLib {
+		debug.Add("rand potion: (%d) chance=%d", roll, t.chance)
+		if roll <= t.chance {
+			name = t.name
+			break
+		}
+	}
+	return newPotion(name)
 }
 
 // === EFFECTS ===========================================================
